a35663b31018f7873a01cf7341ee9cbdd3c4c002
josh <jgraydus@gmail.com>
2024-10-07 21:00:08 -0600

    improvements to random shuffle

--- src/Random.hs
+++ src/Random.hs
@@ -6,25 +6,18 @@ import Control.Monad.Random.Class (MonadRandom(..))
 import Data.Vector.Generic.Mutable qualified as MV
 import Data.Vector.Mutable (MVector)
 
-swap :: MVector s a -> (Int, Int) -> ST s ()
-swap v (i, j) = do
-  iv <- MV.read v i
-  jv <- MV.read v j
-  MV.write v i jv
-  MV.write v j iv
-
--- | create a uniform random permutation (see Fisher-Yates)
+-- | create a uniform random permutation
 randomPermutation :: MonadRandom m => Int -> m [Int]
 randomPermutation n = shuffle [0..n-1]
 
--- | shuffle a given list via a uniform random permutation
+-- | shuffle a given list via a uniform random permutation (see Fisher-Yates algorithm)
 shuffle :: MonadRandom m => [a] -> m [a]
 shuffle xs = do
   let n = length xs
   ps <- forM [0..n-2] $ \m -> getRandomR (m, n-1)
   pure $ runST $ do
     v :: MVector s a <- MV.new n
-    forM_ (zip [0..] xs) $ \(i, x) -> MV.write v i x
-    forM_ (zip [0..] ps) $ swap v
+    forM_ (zip [0..] xs) $ \(i, x) -> MV.unsafeWrite v i x
+    forM_ (zip [0..] ps) $ uncurry (MV.unsafeSwap v)
     MV.foldr' (:) [] v
 
