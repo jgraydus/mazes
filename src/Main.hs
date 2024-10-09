8b9682c427fc16606d0e2319b3e54eac5430a3cf
josh <jgraydus@gmail.com>
2024-10-08 21:18:50 -0600

    add border

--- src/Main.hs
+++ src/Main.hs
@@ -6,35 +6,39 @@ import Diagrams.Backend.SVG.CmdLine
 import Maze
 import Graph
 
-
-
 drawMaze :: Maze -> Diagram B
-drawMaze m = (edges m.graph & fmap mkSeg & mconcat) # lw 3.0
+drawMaze m = (edges m.graph & fmap mkSeg & mconcat) <> border & lw 3.0
   where
     cellSize :: Double = 1.0 / fromIntegral m.width
-    hseg = (P $ V2 (negate cellSize / 2.0) 0.0) ~~ (P $ V2 (cellSize / 2.0) 0.0)
-    vseg = (P $ V2 0.0 (negate cellSize / 2.0)) ~~ (P $ V2 0.0 (cellSize / 2.0))
+    halfCell = cellSize / 2.0
+    hseg = p2 (negate halfCell, 0.0) ~~ p2 (halfCell, 0.0)
+    vseg = p2 (0.0, negate halfCell) ~~ p2 (0.0, halfCell)
 
     row i = i `div` m.width
     col i = i `mod` m.width
 
-    offset_ = (P $ V2 (cellSize / 2.0) (cellSize / 2.0)) - (P $ V2 0.5 0.5)
+    offset_ = p2 (halfCell, halfCell) - p2 (0.5, 0.5)
 
     mkSeg (i, j) = let (ri, ci, rj, cj) = (row i, col i, row j, col j)
-                       x = P $ V2 (fromIntegral ci * cellSize) (fromIntegral ri * cellSize)
-                       y = P $ V2 (fromIntegral cj * cellSize) (fromIntegral rj * cellSize)
+                       x = p2 (fromIntegral ci * cellSize, fromIntegral ri * cellSize)
+                       y = p2 (fromIntegral cj * cellSize, fromIntegral rj * cellSize)
                        p = (x + y) / 2.0 + offset_
                    in position [(p, if ri == rj then vseg else hseg)]
+
+    tl = p2 ((-0.5), 0.5)
+    tr = p2 (0.5, 0.5)
+    bl = p2 ((-0.5), (-0.5))
+    br = p2 (0.5, (-0.5))
+
+    border = (tl ~~ tr) <> (tr ~~ br) <> (br ~~ bl) <> (bl ~~ tl) & pad 1.05
 
 
 main :: IO ()
 main = do
-  let gen = mkStdGen 73
-      m = evalRand (generateMaze 50 50) gen
+  let gen = mkStdGen 9874374
+      m = evalRand (generateMaze 40 40) gen
 
   let d = drawMaze m
 
   mainWith $ d
-
-
 
