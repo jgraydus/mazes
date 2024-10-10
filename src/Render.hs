3068181bf4b33fa51c0fbfe0b05812d1301cd3e3
josh <jgraydus@gmail.com>
2024-10-09 22:31:06 -0600

    fix render bug

--- src/Render.hs
+++ src/Render.hs
@@ -7,12 +7,14 @@ import Graph (edges)
 type C a = (V a ~ V2, N a ~ Double, Renderable (Path V2 Double) a, Backend a V2 Double)
 
 drawMaze :: C a => Maze -> Diagram a
-drawMaze m = (edges m.graph & fmap mkSeg & mconcat) & matteAndFrame
+drawMaze m = innerWalls <> border
              & lw 3.0
-             & lineCap LineCapRound
              & lc black
+             & lineCap LineCapRound
+             & center
+             & pad 1.05
   where
-    cellSize :: Double = 1.0 / fromIntegral m.width
+    cellSize :: Double = 1.0 / fromIntegral (max m.width m.height)
     halfCell = cellSize / 2.0
     hseg = p2 (negate halfCell, 0.0) ~~ p2 (halfCell, 0.0)
     vseg = p2 (0.0, negate halfCell) ~~ p2 (0.0, halfCell)
@@ -20,17 +22,17 @@ drawMaze m = (edges m.graph & fmap mkSeg & mconcat) & matteAndFrame
     row i = i `div` m.width
     col i = i `mod` m.width
 
-    offset_ = p2 (halfCell, halfCell) - p2 (0.5, 0.5)
-
     mkSeg (i, j) = let (ri, ci, rj, cj) = (row i, col i, row j, col j)
                        x = p2 (fromIntegral ci * cellSize, fromIntegral ri * cellSize)
                        y = p2 (fromIntegral cj * cellSize, fromIntegral rj * cellSize)
-                       p = (x + y) / 2.0 + offset_
+                       p = (x + y) / 2.0 + p2 (halfCell, halfCell)
                    in position [(p, if ri == rj then vseg else hseg)]
 
-    uv1 = V2 1.0 0.0
-    uv2 = V2 0.0 1.0
+    innerWalls = edges m.graph & fmap mkSeg & mconcat
 
-    matteAndFrame d = let r = rect (diameter uv1 d) (diameter uv2 d) & fc white & pad 1.02
-                      in (d & center) <> r
+    w = fromIntegral m.width * cellSize
+    h = fromIntegral m.height * cellSize
+
+
+    border = fromVertices [p2 (0.0, 0.0), p2 (w, 0.0), p2 (w, h), p2 (0.0, h), p2 (0.0, 0.0)]
 
